d6fc9bc1a83cb2b15c2b75742e08fbd86f515c31
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2026-01-22 21:44:59 +0530

    added login signup logout functionality

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,14 +1,96 @@
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
 
 const userResolver = {
-  Query: {
-    users: (_, _, { req, res }) => {
-      return users;
+  Mutation: {
+    signUp: async (_, { input }, context) => {
+      try {
+        const { username, name, password, gender } = input;
+
+        if (!username || !name || !password || !gender) {
+          throw new Error("All fiels are mandatory");
+        }
+        if (User.findOne({ username })) {
+          throw new Error("User already exists");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hasedPassword = await bcrypt.hash(password, salt);
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+          username,
+          name,
+          password: hasedPassword,
+          gender:
+            gender.toLowerCase() === "male" ? boyProfilePic : girlProfilePic,
+        });
+        await newUser.save();
+        await context.login(newUser);
+        return newUser;
+      } catch (error) {
+        console.error("Error in creating user: ", error);
+        throw new Error(error.message || "Internal Server Error");
+      }
     },
-    user: (_, { userId }, { req, res }) => {
-      return users.find((user) => user._id === userId);
+    login: async (_, { input }, context) => {
+      try {
+        const { username, passowrd } = input;
+        const { user } = await context.authenticate("graphql-local", {
+          username,
+          passowrd,
+        });
+        await context.login(user);
+        return user;
+      } catch (error) {
+        console.error("Error in login: ", error);
+
+        throw new Error("Internal server error");
+      }
+    },
+    logout: async (_, _, context) => {
+      try {
+        await context.logout();
+        req.session.destroy((err) => {
+          if (err) {
+            console.error("Error destroying session: ", err);
+          }
+        });
+        res.clearCookie("connect.sid");
+
+        return { message: "Logout successful" };
+      } catch (error) {
+        console.error("Error in logout: ", error);
+        throw new Error("Internal server error");
+      }
     },
   },
-  Mutation: {},
+  Query: {
+    authUser: async (_, _context) => {
+      try {
+        const user = await context.getUser();
+        if (!user) {
+          throw new Error("Not authenticated");
+        }
+        return user;
+      } catch (error) {
+        console.error("Error in fetching auth user: ", error);
+        throw new Error("Internal server error");
+      }
+    },
+    user: async (_, { userId }) => {
+      try {
+        const user = await User.findById(userId);
+        if (!user) {
+          throw new Error("User not found");
+        }
+        return user;
+      } catch (error) {
+        console.error("Error in fetching user: ", error);
+        throw new Error(err.meesage || "Internal server error");
+      }
+    },
+  },
+  // TODO=>ADD USER/TRANSACTION Relation
 };
 export default userResolver;
--- backend/typeDefs/user.typeDef.js
+++ backend/typeDefs/user.typeDef.js
@@ -8,7 +8,7 @@ type User {
     gender:String!
 }
 type Query {
-    users:[User!]
+    
     authUser:User!
     user(userId:ID!):User!
 
